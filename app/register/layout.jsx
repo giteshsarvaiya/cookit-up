1f200554325db58ff9be9db7f70662b2ac938989
C14rc <rajeevdewangan10@gmail.com>
2023-06-12 16:40:08 +0530

    register page design

---
+++ app/register/layout.jsx
@@ -0,0 +1,26 @@
+import "@styles/globals.css";
+import { NavLoginPage } from '@components/Nav';
+import React from 'react'
+
+export const metadata = {
+    title: "Register Page",
+  };
+  
+const LoginLayout = ({children}) => {
+  return (
+    <html lang='en'>
+    <body>
+    <NavLoginPage/>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          {children}
+        </main>
+    </body>
+  </html>
+  )
+}
+
+export default LoginLayout;
